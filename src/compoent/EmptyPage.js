010d71b6c27416b156c4efced28f3d523e59158d
이혜원 <sunone2@hunet.co.kr>
2022-09-06 13:56:04 +0900

    add

--- src/compoent/EmptyPage.js
+++ src/compoent/EmptyPage.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function EmptyPage() {
   return (
     <>
-      <h2>잘못된 접근입니다.</h2>
+      <h2>잘못된 접근입니다..</h2>
       <Link to="/">돌아가기</Link>
     </>
   );
