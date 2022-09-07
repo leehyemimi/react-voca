f17e31b19d29c438477c8171bc2b6c9bb425b5ec
이혜원 <sunone2@hunet.co.kr>
2022-09-07 11:26:33 +0900

    add

--- src/compoent/Header.js
+++ src/compoent/Header.js
@@ -1,6 +1,6 @@
 
 import './Header.scss'
-import {Link,useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 function Header(){
 	const navigate = useNavigate();
 	return(
